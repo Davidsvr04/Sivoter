d16684443fddb5bf99dbb50a7dfb69253dc11083
David Santiago Viloria <140551397+Davidsvr04@users.noreply.github.com>
2026-03-22 23:29:11 -0500

    Conexion base de datos

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from sqlalchemy import text
+from app.config import settings
+from app.database.connection import engine
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Corriendo API Sivoter",
+    }
+
+@app.on_event("startup")
+def test_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        print("Conexión exitosa a PostgreSQL")
+    except Exception as e:
+        print("Error de conexión:", e)
